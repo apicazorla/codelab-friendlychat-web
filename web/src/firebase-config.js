b6ed9441e3089862940dc6ebc1d32d4603850625
fiscayquimica <fisicayquimica@telefonica.net>
2023-04-02 13:56:56 +0200

    ok

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -8,6 +8,25 @@
  */
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXWV-e0iCgMJ_3dcCgO_KyZjCp_-57cfs",
+    authDomain: "friendlychat-bf3e8.firebaseapp.com",
+    projectId: "friendlychat-bf3e8",
+    storageBucket: "friendlychat-bf3e8.appspot.com",
+    messagingSenderId: "527520812926",
+    appId: "1:527520812926:web:3a229cec32a937fc6c6d2f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
 };
 
 export function getFirebaseConfig() {
